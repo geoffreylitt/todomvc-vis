b50e265390a287b328491e477642e834fc88a964
Geoffrey Litt <gklitt@gmail.com>
2020-04-29 13:13:36 -0400

    Updates

---
+++ precache-manifest.dbec105eb0ae008fa8f16e6af7e1b5ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5b6ff5dc9b4167423f66d35b9b14053",
+    "url": "/pages/6894-sp20/FP-Program-Execution-Visualization/index.html"
+  },
+  {
+    "revision": "6b24a57707279fbf9cda",
+    "url": "/pages/6894-sp20/FP-Program-Execution-Visualization/static/css/2.8dd801da.chunk.css"
+  },
+  {
+    "revision": "6b24a57707279fbf9cda",
+    "url": "/pages/6894-sp20/FP-Program-Execution-Visualization/static/js/2.5158c329.chunk.js"
+  },
+  {
+    "revision": "0269df3f8b02e26626c7",
+    "url": "/pages/6894-sp20/FP-Program-Execution-Visualization/static/js/main.085b9e87.chunk.js"
+  },
+  {
+    "revision": "1c8fb0e2dade0b210746",
+    "url": "/pages/6894-sp20/FP-Program-Execution-Visualization/static/js/runtime~main.6c763217.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
+
+importScripts(
+  "/pages/6894-sp20/FP-Program-Execution-Visualization/precache-manifest.dbec105eb0ae008fa8f16e6af7e1b5ab.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pages/6894-sp20/FP-Program-Execution-Visualization/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
